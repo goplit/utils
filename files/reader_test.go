d9252612c8f4a4bfe781771827b11831f9908e32
arepin <arepin@cisco.com>
2022-08-21 22:34:41 -0700

    First test optimized

--- files/reader_test.go
+++ files/reader_test.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -9,14 +10,25 @@ func TestReadFileToProduct(t *testing.T) {
 		path string
 	}
 	tests := []struct {
-		name   string
-		args   args
-		cmdSeq []string
+		name         string
+		args         args
+		cmdFuncTyped func(t *testing.T, product TypifyProduct)
 	}{
 		{
 			"Test reading env file",
 			args{path: "test-tools/example.env"},
-			[]string{},
+			func(t *testing.T, product TypifyProduct) {
+				val := product.EquationLineTransforms().GetKey("NO_PROXY").AsCSVList()
+				if val.Error() != nil {
+					t.Errorf("cannot read env file NO_PROXY key due %v", val.AllErrors())
+					return
+				}
+				if !reflect.DeepEqual(val.Value(), []string{".svc.cluster.local", "localhost", "127.0.0.1"}) {
+					t.Errorf("cannot verify env file NO_PROXY key data due %v != %v", val.Value(), []string{".svc.cluster.local", "localhost", "127.0.0.1"})
+					return
+				}
+				t.Logf("successfully read and verified NO_PROXY CSV key in env test file")
+			},
 		},
 	}
 
@@ -25,8 +37,8 @@ func TestReadFileToProduct(t *testing.T) {
 			//if got := ReadFileToProduct(tt.args.path); !reflect.DeepEqual(got, tt.want) {
 			//	t.Errorf("ReadFileToProduct() = %v, want %v", got, tt.want)
 			//}
-			val := ReadFileToProduct(tt.args.path).As().EquationLineTransforms().GetKey("NO_PROXY").AsCSVList().Value()
-			t.Logf("output: %v", val)
+			val := ReadFileToProduct(tt.args.path).As()
+			tt.cmdFuncTyped(t, val)
 		})
 	}
 }
